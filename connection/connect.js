b2f7e6ac5d241545732641fbb5c98006a91de539
hackvick <vickyhasija167@gmail.com>
2022-04-20 14:55:25 +0530

    neww change

--- connection/connect.js
+++ connection/connect.js
@@ -1,8 +1,8 @@
 
   const mongoose = require('mongoose')
-  const url = "mongodb://localhost:27017/codebrew";
+  const url = "mongodb+srv://vicky:vicky0001@cluster0.yazrt.mongodb.net/codebrew?retryWrites=true&w=majority"
+  // "mongodb://localhost:27017/codebrew";
   mongoose.connect(url, { useNewUrlParser: true })
  
   const connect = mongoose.connection;
-
   module.exports = {connect}
